b82183b1fd2645783008028f039cadb25eaae1a4
gehlo <gehlo@Laddu>
2024-02-14 18:06:08 +0530

    third

--- src/Demo.java
+++ src/Demo.java
@@ -1,8 +1,9 @@
 
 public class Demo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("welcome");
 
 	}
 
